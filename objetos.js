522911b1db08df77d0949bfa9841c0bdbc09e57d
melpedernera <138738615+melpedernera@users.noreply.github.com>
2024-05-29 19:38:09 -0300

    first commit

---
+++ objetos.js
@@ -0,0 +1,9 @@
+let producto={
+    nombre:"Camiseta",
+    precio:"$1000",
+    cantidadDisponible:10
+}
+producto.nombre = "Zapatillas"
+producto.categoria = "ropa"
+delete producto.cantidadDisponible
+console.log(producto);
